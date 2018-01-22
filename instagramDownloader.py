ee2dc566cb7a0f4b90c9d17c315a4581f4f898d3
Mayato Francisco <mayato.francisco@icloud.com>
2018-01-22 19:48:28 -0200

    add function setImageClip
    Adiciona a imagem baixada para o clipboard

--- instagramDownloader.py
+++ instagramDownloader.py
@@ -1,8 +1,11 @@
 import ui
 from urllib import request
 from bs4 import BeautifulSoup
+from PIL import Image
 import datetime
 import clipboard
+
+fileName = datetime.datetime.now().strftime("%d-%m-%Y_%H:%M:%S") + '.jpg'
 
 def downloadFile(url):
 	print ('Baixando imagem...')
@@ -11,17 +14,22 @@ def downloadFile(url):
 	soup = BeautifulSoup(htmlSource,'html.parser')
 	metaTag = soup.find_all('meta', {'property':'og:image'})
 	imgURL = metaTag[0]['content']
-	fileName = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.jpg'
 	request.urlretrieve(imgURL, fileName)
 	print ('Imagem salva como: ' + fileName)
-	
+
 def clearClip():
 	clipboard.set('')
+
+def setImageClip():
+	img = Image.open(fileName)
+	img.show()
+	clipboard.set_image(img)
 	
 def main():
 	text = clipboard.get()
 	downloadFile(text)
 	clearClip()
+	setImageClip()
 	
 if __name__ == '__main__':
 	main()
